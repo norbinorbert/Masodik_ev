d21c20959317167d602a45b42e4e70812e8efa6d
norbinorbert <47588133+norbinorbert@users.noreply.github.com>
2024-04-16 18:37:50 +0300

    Add files via upload

---
+++ Grafika/Szem3/GlRectangle.cs
@@ -0,0 +1,120 @@
+﻿using Silk.NET.OpenGL;
+
+namespace Szeminarium1_24_02_17_2
+{
+    internal class GlRectangle
+    {
+        public uint Vao { get; }
+        public uint Vertices { get; }
+        public uint Colors { get; }
+        public uint Indices { get; }
+        public uint IndexArrayLength { get; }
+
+        private GL Gl;
+
+        private GlRectangle(uint vao, uint vertices, uint colors, uint indeces, uint indexArrayLength, GL gl)
+        {
+            this.Vao = vao;
+            this.Vertices = vertices;
+            this.Colors = colors;
+            this.Indices = indeces;
+            this.IndexArrayLength = indexArrayLength;
+            this.Gl = gl;
+        }
+
+        public static unsafe GlRectangle CreateRectangleWithPerpendicularNormals(GL Gl)
+        {
+            uint vao = Gl.GenVertexArray();
+            Gl.BindVertexArray(vao);
+
+            float[] vertexArray = new float[] {
+                -0.5f, 0f, 0f, 0f, 0f, 1f,
+                -0.5f, 2f, 0f, 0f, 0f, 1f,
+                0.5f, 0f, 0f, 0f, 0f, 1f,
+                0.5f, 2f, 0f, 0f, 0f, 1f,
+            };
+
+            float[] colorArray = new float[]{
+                1f, 0f, 0f, 1f,
+                1f, 0f, 0f, 1f,
+                1f, 0f, 0f, 1f,
+                1f, 0f, 0f, 1f,
+            };
+
+            uint[] indexArray = new uint[] {
+                0, 2, 1,
+                2, 3, 1,
+            };
+
+            return RectangleBufferSettings(Gl, vao, vertexArray, colorArray, indexArray);
+        }
+
+        public static unsafe GlRectangle CreateRectangleWithAngledNormals(GL Gl)
+        {
+            uint vao = Gl.GenVertexArray();
+            Gl.BindVertexArray(vao);
+
+            float[] vertexArray = new float[] {
+                -0.5f, 0f, 0f, -(float)( Math.Sin(Math.PI / 18)), 0f, -(float)Math.Cos(Math.PI / 18),
+                -0.5f, 2f, 0f, -(float)( Math.Sin(Math.PI / 18)), 0f, -(float)Math.Cos(Math.PI / 18),
+                0.5f, 0f, 0f, (float)(Math.Sin(Math.PI / 18)), 0f, -(float)Math.Cos(Math.PI / 18),
+                0.5f, 2f, 0f, (float)(Math.Sin(Math.PI / 18)), 0f, -(float)Math.Cos(Math.PI / 18),
+            };
+
+            float[] colorArray = new float[]{
+                1f, 0f, 0f, 1f,
+                1f, 0f, 0f, 1f,
+                1f, 0f, 0f, 1f,
+                1f, 0f, 0f, 1f,
+            };
+
+            uint[] indexArray = new uint[] {
+                0, 2, 1,
+                2, 3, 1,
+            };
+
+            return RectangleBufferSettings(Gl, vao, vertexArray, colorArray, indexArray);
+        }
+
+        private static unsafe GlRectangle RectangleBufferSettings(GL Gl, uint vao, float[] vertexArray, float[] colorArray, uint[] indexArray)
+        {
+            uint offsetPos = 0;
+            uint offsetNormal = offsetPos + (3 * sizeof(float));
+            uint vertexSize = offsetNormal + (3 * sizeof(float));
+
+            uint vertices = Gl.GenBuffer();
+            Gl.BindBuffer(GLEnum.ArrayBuffer, vertices);
+            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)vertexArray.AsSpan(), GLEnum.StaticDraw);
+            Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, vertexSize, (void*)offsetPos);
+            Gl.EnableVertexAttribArray(0);
+
+            Gl.EnableVertexAttribArray(2);
+            Gl.VertexAttribPointer(2, 3, VertexAttribPointerType.Float, false, vertexSize, (void*)offsetNormal);
+
+            uint colors = Gl.GenBuffer();
+            Gl.BindBuffer(GLEnum.ArrayBuffer, colors);
+            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray.AsSpan(), GLEnum.StaticDraw);
+            Gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, null);
+            Gl.EnableVertexAttribArray(1);
+
+            uint indices = Gl.GenBuffer();
+            Gl.BindBuffer(GLEnum.ElementArrayBuffer, indices);
+            Gl.BufferData(GLEnum.ElementArrayBuffer, (ReadOnlySpan<uint>)indexArray.AsSpan(), GLEnum.StaticDraw);
+
+            // release array buffer
+            Gl.BindBuffer(GLEnum.ArrayBuffer, 0);
+            uint indexArrayLength = (uint)indexArray.Length;
+
+            return new GlRectangle(vao, vertices, colors, indices, indexArrayLength, Gl);
+        }
+
+        internal void Release()
+        {
+            // always unbound the vertex buffer first, so no halfway results are displayed by accident
+            Gl.DeleteBuffer(Vertices);
+            Gl.DeleteBuffer(Colors);
+            Gl.DeleteBuffer(Indices);
+            Gl.DeleteVertexArray(Vao);
+        }
+    }
+}
---
+++ Grafika/Szem3/Program.cs
@@ -0,0 +1,501 @@
+﻿using ImGuiNET;
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.OpenGL.Extensions.ImGui;
+using Silk.NET.Windowing;
+
+namespace Szeminarium1_24_02_17_2
+{
+    internal static class Program
+    {
+        private static CameraDescriptor cameraDescriptor = new();
+
+        private static CubeArrangementModel cubeArrangementModel = new();
+
+        private static IWindow window;
+
+        private static IInputContext inputContext;
+
+        private static GL Gl;
+
+        private static ImGuiController controller;
+
+        private static uint PhongProgram;
+        private static uint GourardProgram;
+        private static uint activeProgram;
+
+        private static int NUMBER_OF_RECTANGLES = 18;
+
+        private static GlRectangle glTopRectangle;
+
+        private static GlRectangle glBottomRectangle;
+
+        private static float Shininess = 50;
+
+        private const string ModelMatrixVariableName = "uModel";
+        private const string NormalMatrixVariableName = "uNormal";
+        private const string ViewMatrixVariableName = "uView";
+        private const string ProjectionMatrixVariableName = "uProjection";
+
+        private static readonly string PhongVertexShaderSource = @"
+        #version 330 core
+        layout (location = 0) in vec3 vPos;
+		layout (location = 1) in vec4 vCol;
+        layout (location = 2) in vec3 vNorm;
+
+        uniform mat4 uModel;
+        uniform mat3 uNormal;
+        uniform mat4 uView;
+        uniform mat4 uProjection;
+
+		out vec4 outCol;
+        out vec3 outNormal;
+        out vec3 outWorldPosition;
+        
+        void main()
+        {
+			outCol = vCol;
+            gl_Position = uProjection*uView*uModel*vec4(vPos.x, vPos.y, vPos.z, 1.0);
+            outNormal = uNormal*vNorm;
+            outWorldPosition = vec3(uModel*vec4(vPos.x, vPos.y, vPos.z, 1.0));
+        }
+        ";
+
+        private static readonly string GourardVertexShaderSource = @"
+        #version 330 core
+        layout (location = 0) in vec3 vPos;
+        layout (location = 1) in vec4 vCol;
+        layout (location = 2) in vec3 vNorm;
+            
+        uniform mat4 uModel;
+        uniform mat3 uNormal;
+        uniform mat4 uView;
+        uniform mat4 uProjection;
+        uniform vec3 lightColor;
+        uniform vec3 lightPos;
+        uniform vec3 viewPos;
+        uniform float shininess;
+
+        out vec4 vColor;
+
+        void main()
+        {
+            gl_Position = uProjection * uView * uModel * vec4(vPos.x, vPos.y, vPos.z, 1.0);
+
+            vec3 WorldPosition = vec3(uModel * vec4(vPos.x, vPos.y, vPos.z, 1.0));
+
+            float ambientStrength = 0.2;
+            vec3 ambient = ambientStrength * lightColor;
+
+            float diffuseStrength = 0.3;
+            vec3 norm = normalize(uNormal*vNorm);
+            vec3 lightDir = normalize(lightPos - WorldPosition);
+            float diff = max(dot(norm, lightDir), 0.0);
+            vec3 diffuse = diff * lightColor * diffuseStrength;
+
+            float specularStrength = 0.5;
+            vec3 viewDir = normalize(viewPos - WorldPosition);
+            vec3 reflectDir = reflect(-lightDir, norm);
+            float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess) / max(dot(norm,viewDir), -dot(norm,lightDir));
+            vec3 specular = specularStrength * spec * lightColor;  
+
+            vec3 result = (ambient + diffuse + specular) * vCol.xyz;
+            vColor = vec4(result, vCol.z);
+        }
+        ";
+
+        private const string LightColorVariableName = "lightColor";
+        private const string LightPositionVariableName = "lightPos";
+        private const string ViewPosVariableName = "viewPos";
+        private const string ShininessVariableName = "shininess";
+
+        private static readonly string PhongFragmentShaderSource = @"
+        #version 330 core
+        
+        uniform vec3 lightColor;
+        uniform vec3 lightPos;
+        uniform vec3 viewPos;
+        uniform float shininess;
+
+        out vec4 FragColor;
+
+		in vec4 outCol;
+        in vec3 outNormal;
+        in vec3 outWorldPosition;
+
+        void main()
+        {
+            float ambientStrength = 0.2;
+            vec3 ambient = ambientStrength * lightColor;
+
+            float diffuseStrength = 0.3;
+            vec3 norm = normalize(outNormal);
+            vec3 lightDir = normalize(lightPos - outWorldPosition);
+            float diff = max(dot(norm, lightDir), 0.0);
+            vec3 diffuse = diff * lightColor * diffuseStrength;
+
+            float specularStrength = 0.5;
+            vec3 viewDir = normalize(viewPos - outWorldPosition);
+            vec3 reflectDir = reflect(-lightDir, norm);
+            float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess) / max(dot(norm,viewDir), -dot(norm,lightDir));
+            vec3 specular = specularStrength * spec * lightColor;  
+
+            vec3 result = (ambient + diffuse + specular) * outCol.xyz;
+            FragColor = vec4(result, outCol.w);
+        }
+        ";
+
+        private static readonly string GourardFragmentShaderSource = @"
+        #version 330 core
+
+        in vec4 vColor;
+
+        void main()
+        {
+            gl_FragColor = vColor;
+        }
+        ";
+
+        private static int radioButtonIndex = 0;
+
+        static void Main(string[] args)
+        {
+            WindowOptions windowOptions = WindowOptions.Default;
+            windowOptions.Title = "3 szeminárium";
+            windowOptions.Size = new Vector2D<int>(500, 500);
+
+            // on some systems there is no depth buffer by default, so we need to make sure one is created
+            windowOptions.PreferredDepthBufferBits = 24;
+
+            window = Window.Create(windowOptions);
+
+            window.Load += Window_Load;
+            window.Update += Window_Update;
+            window.Render += Window_Render;
+            window.Closing += Window_Closing;
+
+            window.Run();
+        }
+
+        private static void Window_Load()
+        {
+            //Console.WriteLine("Load");
+
+            // set up input handling
+            inputContext = window.CreateInput();
+            foreach (var keyboard in inputContext.Keyboards)
+            {
+                keyboard.KeyDown += Keyboard_KeyDown;
+            }
+
+            Gl = window.CreateOpenGL();
+
+            controller = new ImGuiController(Gl, window, inputContext);
+
+            // Handle resizes
+            window.FramebufferResize += s =>
+            {
+                // Adjust the viewport to the new window size
+                Gl.Viewport(s);
+            };
+
+
+            Gl.ClearColor(System.Drawing.Color.White);
+
+            SetUpObjects();
+
+            LinkProgram(PhongVertexShaderSource, PhongFragmentShaderSource, ref PhongProgram);
+            LinkProgram(GourardVertexShaderSource, GourardFragmentShaderSource, ref GourardProgram);
+            activeProgram = PhongProgram;
+
+            //Gl.Enable(EnableCap.CullFace);
+
+            Gl.Enable(EnableCap.DepthTest);
+            Gl.DepthFunc(DepthFunction.Lequal);
+        }
+
+        private static void LinkProgram(string vertexShaderSource, string fragmentShaderSource, ref uint program)
+        {
+            uint vshader = Gl.CreateShader(ShaderType.VertexShader);
+            uint fshader = Gl.CreateShader(ShaderType.FragmentShader);
+
+            Gl.ShaderSource(vshader, vertexShaderSource);
+            Gl.CompileShader(vshader);
+            Gl.GetShader(vshader, ShaderParameterName.CompileStatus, out int vStatus);
+            if (vStatus != (int)GLEnum.True)
+                throw new Exception("Vertex shader failed to compile: " + Gl.GetShaderInfoLog(vshader));
+
+            Gl.ShaderSource(fshader, fragmentShaderSource);
+            Gl.CompileShader(fshader);
+
+            program = Gl.CreateProgram();
+            Gl.AttachShader(program, vshader);
+            Gl.AttachShader(program, fshader);
+            Gl.LinkProgram(program);
+            Gl.GetProgram(program, GLEnum.LinkStatus, out var status);
+            if (status == 0)
+            {
+                Console.WriteLine($"Error linking shader {Gl.GetProgramInfoLog(program)}");
+            }
+            Gl.DetachShader(program, vshader);
+            Gl.DetachShader(program, fshader);
+            Gl.DeleteShader(vshader);
+            Gl.DeleteShader(fshader);
+        }
+
+        private static void Keyboard_KeyDown(IKeyboard keyboard, Key key, int arg3)
+        {
+            switch (key)
+            {
+                case Key.W:
+                    cameraDescriptor.MoveForward();
+                    break;
+                case Key.S:
+                    cameraDescriptor.MoveBackward();
+                    break;
+                case Key.A:
+                    cameraDescriptor.MoveLeft();
+                    break;
+                case Key.D:
+                    cameraDescriptor.MoveRight();
+                    break;
+                case Key.Space:
+                    cameraDescriptor.MoveUp();
+                    break;
+                case Key.ControlLeft:
+                    cameraDescriptor.MoveDown();
+                    break;
+                case Key.Left:
+                    cameraDescriptor.LookLeft();
+                    break;
+                case Key.Right:
+                    cameraDescriptor.LookRight();
+                    break;
+                case Key.Up:
+                    cameraDescriptor.LookUp();
+                    break;
+                case Key.Down:
+                    cameraDescriptor.LookDown();
+                    break;
+                case Key.Backspace:
+                    cubeArrangementModel.AnimationEnabeld = !cubeArrangementModel.AnimationEnabeld;
+                    break;
+            }
+        }
+
+        private static void Window_Update(double deltaTime)
+        {
+            //Console.WriteLine($"Update after {deltaTime} [s].");
+            // multithreaded
+            // make sure it is threadsafe
+            // NO GL calls
+            cubeArrangementModel.AdvanceTime(deltaTime);
+
+            controller.Update((float)deltaTime);
+        }
+
+        private static unsafe void Window_Render(double deltaTime)
+        {
+            Gl.Clear(ClearBufferMask.ColorBufferBit);
+            Gl.Clear(ClearBufferMask.DepthBufferBit);
+
+            //ImGuiNET.ImGui.ShowDemoWindow();
+            ImGui.Begin("Lighting properties",
+                ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoTitleBar);
+            ImGui.SliderFloat("Shininess", ref Shininess, 1, 200);
+            if (ImGui.RadioButton("Phong", ref radioButtonIndex, 0))
+            {
+                activeProgram = PhongProgram;
+            }
+
+            if (ImGui.RadioButton("Gourard", ref radioButtonIndex, 1))
+            {
+                activeProgram = GourardProgram;
+            }
+            ImGui.End();
+
+            Gl.UseProgram(activeProgram);
+
+            SetViewMatrix();
+            SetProjectionMatrix();
+
+            SetLightColor();
+            SetLightPosition();
+            SetViewerPosition();
+            
+            SetShininess();
+
+            DrawRectangles();
+
+            controller.Render();
+        }
+
+        private static unsafe void SetLightColor()
+        {
+            int location = Gl.GetUniformLocation(activeProgram, LightColorVariableName);
+
+            if (location == -1)
+            {
+                throw new Exception($"{LightColorVariableName} uniform not found on shader.");
+            }
+
+            Gl.Uniform3(location, 1f, 1f, 1f);
+            CheckError();
+        }
+
+        private static unsafe void SetLightPosition()
+        {
+            int location = Gl.GetUniformLocation(activeProgram, LightPositionVariableName);
+
+            if (location == -1)
+            {
+                throw new Exception($"{LightPositionVariableName} uniform not found on shader.");
+            }
+
+            Gl.Uniform3(location, cameraDescriptor.Position.X, cameraDescriptor.Position.Y, cameraDescriptor.Position.Z);
+            CheckError();
+        }
+
+        private static unsafe void SetViewerPosition()
+        {
+            int location = Gl.GetUniformLocation(activeProgram, ViewPosVariableName);
+
+            if (location == -1)
+            {
+                throw new Exception($"{ViewPosVariableName} uniform not found on shader.");
+            }
+
+            Gl.Uniform3(location, cameraDescriptor.Position.X, cameraDescriptor.Position.Y, cameraDescriptor.Position.Z);
+            CheckError();
+        }
+
+        private static unsafe void SetShininess()
+        {
+            int location = Gl.GetUniformLocation(activeProgram, ShininessVariableName);
+
+            if (location == -1)
+            {
+                throw new Exception($"{ShininessVariableName} uniform not found on shader.");
+            }
+
+            Gl.Uniform1(location, Shininess);
+            CheckError();
+        }
+
+        private static unsafe void DrawRectangles()
+        {
+            for (int i = 0; i < NUMBER_OF_RECTANGLES; i++)
+            {
+                double translationX = 0;
+                double translationZ = 0;
+                double angle = Math.PI / 9 * i;
+
+
+                var rotation = Matrix4X4.CreateRotationY((float)angle);
+                if (i != 0)
+                {
+                    // - 1/2 + cos(20) + cos(40) + ... + cos(angle) / 2
+                    translationX += Math.Cos(angle) / 2 - 0.5;
+                    // sin(20) + sin(40) + ... + sin(angle) / 2
+                    translationZ += Math.Sin(angle) / 2;
+                    for (int j = 0; j < i; j++)
+                    {
+                        translationX += Math.Cos(Math.PI / 9 * j);
+                        translationZ += Math.Sin(Math.PI / 9 * j);
+                    }
+                }
+
+                var translation = Matrix4X4.CreateTranslation(new Vector3D<float>((float)-translationX, 0, (float)translationZ));
+                var modelMatrix = rotation * translation;
+                SetModelMatrix(modelMatrix);
+                Gl.BindVertexArray(glTopRectangle.Vao);
+                Gl.DrawElements(GLEnum.Triangles, glTopRectangle.IndexArrayLength, GLEnum.UnsignedInt, null);
+                Gl.BindVertexArray(0);
+
+                translation = Matrix4X4.CreateTranslation(new Vector3D<float>((float)-translationX, -2, (float)translationZ));
+                modelMatrix = rotation * translation;
+                SetModelMatrix(modelMatrix);
+                Gl.BindVertexArray(glBottomRectangle.Vao);
+                Gl.DrawElements(GLEnum.Triangles, glBottomRectangle.IndexArrayLength, GLEnum.UnsignedInt, null);
+                Gl.BindVertexArray(0);
+            }
+        }
+
+        private static unsafe void SetModelMatrix(Matrix4X4<float> modelMatrix)
+        {
+            int location = Gl.GetUniformLocation(activeProgram, ModelMatrixVariableName);
+            if (location == -1)
+            {
+                throw new Exception($"{ModelMatrixVariableName} uniform not found on shader.");
+            }
+
+            Gl.UniformMatrix4(location, 1, false, (float*)&modelMatrix);
+            CheckError();
+
+            var modelMatrixWithoutTranslation = new Matrix4X4<float>(modelMatrix.Row1, modelMatrix.Row2, modelMatrix.Row3, modelMatrix.Row4);
+            modelMatrixWithoutTranslation.M41 = 0;
+            modelMatrixWithoutTranslation.M42 = 0;
+            modelMatrixWithoutTranslation.M43 = 0;
+            modelMatrixWithoutTranslation.M44 = 1;
+
+            Matrix4X4<float> modelInvers;
+            Matrix4X4.Invert<float>(modelMatrixWithoutTranslation, out modelInvers);
+            Matrix3X3<float> normalMatrix = new Matrix3X3<float>(Matrix4X4.Transpose(modelInvers));
+            location = Gl.GetUniformLocation(activeProgram, NormalMatrixVariableName);
+            if (location == -1)
+            {
+                throw new Exception($"{NormalMatrixVariableName} uniform not found on shader.");
+            }
+            Gl.UniformMatrix3(location, 1, false, (float*)&normalMatrix);
+            CheckError();
+        }
+
+        private static unsafe void SetUpObjects()
+        {
+            glTopRectangle = GlRectangle.CreateRectangleWithPerpendicularNormals(Gl);
+            glBottomRectangle = GlRectangle.CreateRectangleWithAngledNormals(Gl);
+        }
+
+        private static void Window_Closing()
+        {
+            glTopRectangle.Release();
+            glBottomRectangle.Release();
+        }
+
+        private static unsafe void SetProjectionMatrix()
+        {
+            var projectionMatrix = Matrix4X4.CreatePerspectiveFieldOfView<float>((float)Math.PI / 4f, 1024f / 768f, 0.1f, 100);
+            int location = Gl.GetUniformLocation(activeProgram, ProjectionMatrixVariableName);
+
+            if (location == -1)
+            {
+                throw new Exception($"{ViewMatrixVariableName} uniform not found on shader.");
+            }
+
+            Gl.UniformMatrix4(location, 1, false, (float*)&projectionMatrix);
+            CheckError();
+        }
+
+        private static unsafe void SetViewMatrix()
+        {
+            var viewMatrix = Matrix4X4.CreateLookAt(cameraDescriptor.Position, cameraDescriptor.Target, cameraDescriptor.UpVector);
+            int location = Gl.GetUniformLocation(activeProgram, ViewMatrixVariableName);
+
+            if (location == -1)
+            {
+                throw new Exception($"{ViewMatrixVariableName} uniform not found on shader.");
+            }
+
+            Gl.UniformMatrix4(location, 1, false, (float*)&viewMatrix);
+            CheckError();
+        }
+
+        public static void CheckError()
+        {
+            var error = (ErrorCode)Gl.GetError();
+            if (error != ErrorCode.NoError)
+                throw new Exception("GL.GetError() returned " + error.ToString());
+        }
+    }
+}
